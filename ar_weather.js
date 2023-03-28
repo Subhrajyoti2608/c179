357bea935b545340270966e3f61ed4192555318c
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-03-28 16:52:28 +0530

    Add files via upload

---
+++ ar_weather.js
@@ -0,0 +1,25 @@
+let coordinates = {}
+
+$(document).ready(function(){
+    get_coordinates()
+})
+
+function get_coordinates(){
+    var searchParams = URLSearchParams(window.location.search)
+
+    if(searchParams.has("source")&&searchParams.has("destination")){
+
+        var source = searchParams.get("source")
+
+        var destination = searchParams.get("destination")
+
+        coordinates.source_lat = source.split(";")[0]
+        coordinates.source_lon = source.split(";")[1]
+
+        coordinates.destination_lat = destination.split(";")[0]
+        coordinates.destination_lon = destination.split(";")[1]
+    }else{
+        alert("Coordinates not selected")
+        window.history.back()
+    }
+}
